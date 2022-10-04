0d464225ac9f420e57712b35cda0f50d98d09930
Mars <=>
2022-10-04 15:30:19 +0700

    some audio

---
+++ scripts/control.js
@@ -0,0 +1,17 @@
+const anhDech = document.getElementById("anhDech");
+const hayDen = document.getElementById("hayDen");
+const silent = document.getElementById("silentOpen");
+
+anhDech.addEventListener("play", () => {
+    setTimeout(() => {
+        anhDech.currentTime = 0;
+    }, 6500);
+    setTimeout(() => {
+        anhDech.currentTime = 0;
+    }, 11000);
+});
+
+hayDen.currentTime = 88;
+hayDen.addEventListener("pause", () => {
+    hayDen.currentTime = 88;
+});
