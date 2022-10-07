40bd866cbfd08706aba18914b086d71a738198e9
dylululu <duynhatluu2003@gmail.com>
2022-10-07 19:37:33 +0700

    m

--- scripts/control.js
+++ scripts/control.js
@@ -1,15 +1,15 @@
-const anhDech = document.getElementById("anhDech");
+// const anhDech = document.getElementById("anhDech");
 const hayDen = document.getElementById("hayDen");
-const silent = document.getElementById("silentOpen");
+// const silent = document.getElementById("silentOpen");
 
-anhDech.addEventListener("play", () => {
-    setTimeout(() => {
-        anhDech.currentTime = 0;
-    }, 6500);
-    setTimeout(() => {
-        anhDech.currentTime = 0;
-    }, 11000);
-});
+// anhDech.addEventListener("play", () => {
+//     setTimeout(() => {
+//         anhDech.currentTime = 0;
+//     }, 6500);
+//     setTimeout(() => {
+//         anhDech.currentTime = 0;
+//     }, 11000);
+// });
 
 hayDen.currentTime = 88;
 hayDen.addEventListener("pause", () => {
