3dbeb5e413e1669b8ca58274741df128075f38ce
Mars <=>
2022-10-08 20:36:43 +0700

    control library

--- scripts/control.js
+++ scripts/control.js
@@ -1,17 +1,3 @@
-// const anhDech = document.getElementById("anhDech");
-const hayDen = document.getElementById("hayDen");
-// const silent = document.getElementById("silentOpen");
+audioStartsAt("hayDen", 88);
 
-// anhDech.addEventListener("play", () => {
-//     setTimeout(() => {
-//         anhDech.currentTime = 0;
-//     }, 6500);
-//     setTimeout(() => {
-//         anhDech.currentTime = 0;
-//     }, 11000);
-// });
-
-hayDen.currentTime = 88;
-hayDen.addEventListener("pause", () => {
-    hayDen.currentTime = 88;
-});
+// pauseAfterSeconds("bigCityBoy", 5);
