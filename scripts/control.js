6af21d78a4c82cdace4236959f4e3deae2b55c5f
dylululu <duynhatluu2003@gmail.com>
2022-10-09 19:20:32 +0700

    s

--- scripts/control.js
+++ scripts/control.js
@@ -15,3 +15,35 @@ hayDen.currentTime = 88;
 hayDen.addEventListener("pause", () => {
     hayDen.currentTime = 88;
 });
+const LaConGai = document.getElementById("LaConGai");
+
+LaConGai.currentTime = 68;
+LaConGai.addEventListener("pause", () => {
+    LaConGai.currentTime = 68;
+});
+
+const BiThuong = document.getElementById("BiThuong");
+
+BiThuong.currentTime = 1;
+BiThuong.addEventListener("pause", () => {
+    BiThuong.currentTime = 1;
+});
+
+const MaiDetTiNi = document.getElementById("MaiDetTiNi");
+
+MaiDetTiNi.currentTime = 5;
+MaiDetTiNi.addEventListener("pause", () => {
+    MaiDetTiNi.currentTime = 5;
+});
+
+const TuThoai = document.getElementById("TuThoai");
+
+TuThoai.currentTime = 53;
+TuThoai.addEventListener("pause", () => {
+    TuThoai.currentTime = 53;
+});
+
+Slap.currentTime = 1;
+Slap.addEventListener("pause", () => {
+    Slap.currentTime = 1;
+});
