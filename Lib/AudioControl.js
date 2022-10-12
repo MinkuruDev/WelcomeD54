563abdef6a8f73dfe9a0e46a9ecdf821d50e65d5
dylululu <duynhatluu2003@gmail.com>
2022-10-12 21:14:54 +0700

    m

--- Lib/AudioControl.js
+++ Lib/AudioControl.js
@@ -20,3 +20,29 @@ function audioStartsAt(audioID, seconds){
 function playAudio(audioID){
     document.getElementById(audioID).play();
 }
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+function tangDan(audioID, start = 0.1, end = 1, delta = 0.5){
+    let audio = document.getElementById(audioID);
+    audio.volume = start;
+    
+    audio.addEventListener("play", async () => {
+        while(audio.volume <= end){
+            await sleep(delta * 1000);
+            audio.volume += 0.1;
+        }
+    });
+}
+
+function giamDan(audioID, start = 1, end = 0, delta = 0.5){
+    let audio = document.getElementById(audioID);
+    audio.volume = start;
+    
+    audio.addEventListener("play", async () => {
+        while(audio.volume >= end){
+            await sleep(delta * 1000);
+            audio.volume -= 0.1;
+        }
+    });
+}
--- scripts/control.js
+++ scripts/control.js
@@ -1,10 +1,10 @@
 audioStartsAt("hayDen", 88);
+tangDan("hayDen")
 
-audioStartsAt("BiThuong", 1);
+audioStartsAt("BiThuong", 3);
+tangDan("BiThuong");
 
 audioStartsAt("MaiDetTiNi", 5.5);
-
-audioStartsAt("TuThoai", 53);
 
 audioStartsAt("Slap", 1);
 
@@ -12,5 +12,13 @@ audioStartsAt("LaConGai", 68);
 
 audioStartsAt("3Sung", 3);
 
-audioStartsAt("DuocRoi", 1);
+audioStartsAt("DuocRoi", 1.6);
+
+audioStartsAt("DauLung", 0.5);
+
+audioStartsAt("BaoTap", 1);
+
+audioStartsAt("TuThoai", 53);
+
+
 
