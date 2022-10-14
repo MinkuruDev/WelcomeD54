646b0653b815047f5872c19e67dd119507ba879c
dylululu <duynhatluu2003@gmail.com>
2022-10-14 11:00:29 +0700

    m

--- scripts/control.js
+++ scripts/control.js
@@ -2,14 +2,14 @@ audioStartsAt("hayDen", 88);
 tangDan("hayDen")
 
 audioStartsAt("BiThuong", 3);
-tangDan("BiThuong");
+giamDan("BiThuong",0.8,0,0.01,0.5);
 
 audioStartsAt("MaiDetTiNi", 5.5);
 
 audioStartsAt("Slap", 1.1);
 
 audioStartsAt("LaConGai", 68);
-giamDan("LaConGai", 1, 0.5);
+//giamDan("LaConGai", 1, 0.5);
 
 
 audioStartsAt("3Sung", 3);
@@ -21,16 +21,26 @@ audioStartsAt("DauLung", 0.5);
 audioStartsAt("BaoTap", 1);
 
 audioStartsAt("TuThoai", 53);
+giamDan("TuThoai",1,0,0.01,0.5)
 
-pauseAfterSeconds("BinhNguyen",10 );
 
-pauseAfterSeconds("DauLung",6 );
+pauseAfterSeconds("BinhNguyen",8 );
+
+pauseAfterSeconds("DauLung",3 );
 
 pauseAfterSeconds("hayDen",20 );
 
-// pauseAfterSeconds("DauLung",10 );
+pauseAfterSeconds("Waiting",14 );
 
+pauseAfterSeconds("DuocRoi", 10 );
 
+pauseAfterSeconds("LaConGai", 5 );
 
+pauseAfterSeconds("NK1", 5 );
 
+pauseAfterSeconds("NK2", 5 );
+
+setVolume("TuThoai", 0.7)
+
+pauseAfterSeconds("bigCity", 1.5 );
 
