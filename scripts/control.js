da9aecb88097079b9f188a02cc9ac99fc1d1f907
dylululu <duynhatluu2003@gmail.com>
2022-10-25 21:05:46 +0700

    m

--- scripts/control.js
+++ scripts/control.js
@@ -1,46 +1,84 @@
 audioStartsAt("hayDen", 88);
 tangDan("hayDen")
 
-audioStartsAt("BiThuong", 3);
-giamDan("BiThuong",0.8,0,0.01,0.5);
+audioStartsAt("BiThuong", 1);
+// pauseAfterSeconds("BiThuong",2,0.02,0.7);
 
-audioStartsAt("MaiDetTiNi", 5.5);
+ audioStartsAt("MaiDetTiNi", 5.5);
 
-audioStartsAt("Slap", 1.1);
+// audioStartsAt("Slap", 1.1);
 
-audioStartsAt("LaConGai", 68);
-//giamDan("LaConGai", 1, 0.5);
+audioStartsAt("Talker", 27  );
+pauseAfterSeconds("Talker",3)
 
 
 audioStartsAt("3Sung", 3);
 
 audioStartsAt("DuocRoi", 1.6);
 
-audioStartsAt("DauLung", 0.5);
+// audioStartsAt("DauLung", 0.5);
 
-audioStartsAt("BaoTap", 1);
+audioStartsAt("ConMuc", 17);
+pauseAfterSeconds("ConMuc",6)
+
+pauseAfterSeconds("Nicky",26)
+// audioStartsAt("Nicky", 45);
+
+
+
+audioStartsAt("BaoTap", 0);
+pauseAfterSeconds("BaoTap",4)
+
 
 audioStartsAt("TuThoai", 53);
-giamDan("TuThoai",1,0,0.01,0.5)
+pauseAfterSeconds("TuThoai",11,0.01,0.3)
 
 
-pauseAfterSeconds("BinhNguyen",8 );
+// pauseAfterSeconds("BinhNguyen",8 );
 
-pauseAfterSeconds("DauLung",3 );
+// pauseAfterSeconds("DauLung",1 );
 
 pauseAfterSeconds("hayDen",20 );
 
 pauseAfterSeconds("Waiting",14 );
 
-pauseAfterSeconds("DuocRoi", 10 );
+pauseAfterSeconds("DuocRoi", 5 );
 
-pauseAfterSeconds("LaConGai", 5 );
+// pauseAfterSeconds("LaConGai", 5 );
 
-pauseAfterSeconds("NK1", 5 );
+pauseAfterSeconds("NK1", 2 );
 
-pauseAfterSeconds("NK2", 5 );
+pauseAfterSeconds("NK2", 2 );
 
-setVolume("TuThoai", 0.7)
+setVolume("TuThoai", 0.8)
 
-pauseAfterSeconds("bigCity", 1.5 );
+// pauseAfterSeconds("bigCity", 1.5 );
 
+pauseAfterSeconds("ThienTruc",10);
+
+// audioStartsAt("HieuUng1", 195);
+
+pauseAfterSeconds("VuiNhon",5,0.02,0.4);
+
+// audioStartsAt("NaNi", 1.4);
+
+// pauseAfterSeconds("KK",0,0.03,0.3);
+
+pauseAfterSeconds("ChuyenCanh1",2,0.05,1);
+audioStartsAt("ChuyenCanh1", 5);
+
+pauseAfterSeconds("ChuyenCanh2",8);
+audioStartsAt("ChuyenCanh2", 5);
+
+// audioStartsAt("Fall", 15.3);
+
+// audioStartsAt("QuetNha", 0.5);
+
+
+// pauseAfterSeconds("DanCa",10);
+
+pauseAfterSeconds("QuetNha",10);
+
+audioStartsAt("Confuse", 3);
+
+pauseAfterSeconds("Wind",0);
